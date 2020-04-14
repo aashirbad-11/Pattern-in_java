11bdde5b341cfd86a305cfa72de7cead52fca607
Aashirbad Maharana <aashirbadmaharana11@gmail.com>
2020-04-14 23:52:09 +0530

    day 1 commit

---
+++ java01.java
@@ -0,0 +1,14 @@
+
+public class java01 {
+    public static void main(String[] args) {
+        System.out.println("Jay Maa Jagannath");
+        for(int i = 0; i <= 5; i ++)
+        {
+            for ( int j = 0; j <= 5; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ java04.java
@@ -0,0 +1,19 @@
+
+public class java04 {
+public static void main(String[] args) {
+    for(char i = 'A';i <= 'E'; i ++)
+    {
+        for(int j = 1; j <= 5; j++)
+        {
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+}
+}
+
+// AAAAA
+// BBBBB
+// CCCCC
+// DDDDD
+// EEEEE
